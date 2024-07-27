23e05f8b10e74adbccbe3a0297739dea47ad0fea
Satyam-Pandey677 <satyampandey0241@gmail.com>
2024-07-27 17:43:14 +0530

    Save Changes

---
+++ Email validator/script.js
@@ -0,0 +1,38 @@
+let submit = document.getElementById("submit");
+let card1 = document.getElementById("card1");
+let card2 = document.getElementById("card2")
+let input = document.getElementById("email").value;
+let feedback = document.getElementById("feedback");
+let email_print = document.getElementById("email_print")
+let close_Btn = document.getElementById("btn")
+
+
+function checkinput() {
+    let input = document.getElementById("email").value;
+    arr = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(arr.test(input) ) {
+        feedback.textContent ="";
+    }else{
+        feedback.textContent = "valid email required";
+        input.classList.add("color")
+    }
+}
+
+
+submit.addEventListener("click",(e)=>{
+    let input = document.getElementById("email").value;
+    let email_print = document.getElementById("email_print");
+
+    e.preventDefault()
+
+    card1.style.display = "none"
+    card2.style.display ="block"
+    email_print.textContent =input;
+})
+
+close_Btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    card1.style.display = "flex"
+    card2.style.display = "none"
+})
